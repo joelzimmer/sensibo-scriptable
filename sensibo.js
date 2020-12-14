425846812f7413a7e0d3e4424655c987bf70be5b
Joel Zimmer <joel.zimmer@gmail.com>
2020-12-13 23:19:31 -0500

    Make the script run in Scriptable

--- sensibo.js
+++ sensibo.js
@@ -1,6 +1,8 @@
-const fetch = require("node-fetch");
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-blue; icon-glyph: magic-wand;
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = "";
 const BASE_PATH = "https://home.sensibo.com/api/v2";
 
 const URLS = {
@@ -10,12 +12,9 @@ const URLS = {
 };
 
 const getData = async url => {
-  try {
-    const response = await fetch(url);
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-  }
+  const request = new Request(url);
+  const response = await request.loadJSON();
+  return response;
 };
 
 const getSensiboData = async () => {
@@ -34,7 +33,67 @@ const getSensiboData = async () => {
     })
   );
 
-  console.log(podData);
+  return podData;
 };
 
-getSensiboData();
+async function run() {
+  const listWidget = new ListWidget();
+  listWidget.setPadding(10, 10, 10, 10);
+
+  try {
+    const sensiboData = await getSensiboData();
+
+    if (!sensiboData) {
+      throw "No Data.";
+    }
+
+    console.log({ sensiboData });
+
+    const textColor = Color.dynamic(
+      new Color('313131'),
+      new Color('ffffff')
+    )
+
+    sensiboData.forEach(({ name, temperature, humidity }) => {
+      const nameLabel = listWidget.addText(name.toUpperCase());
+      nameLabel.textColor = textColor;
+      nameLabel.font = Font.semiboldSystemFont(15);
+      nameLabel.minimumScaleFactor = 0.3;
+
+      const temperatureStack = listWidget.addStack();
+      temperatureStack.layoutHorizontally();
+
+      const farenheit = (temperature * 9/5) + 32;
+      const farenheitRounded = Math.round(farenheit * 10) / 10;
+
+      const temperatureContent = temperatureStack.addText(`${farenheitRounded}ºF`);
+      temperatureContent.textColor = textColor;
+      temperatureContent.font = Font.semiboldSystemFont(20);
+
+      temperatureStack.addSpacer(null);
+      
+      const humidityContent = temperatureStack.addText(`${humidity}%`);
+      humidityContent.textColor = textColor;
+      humidityContent.font = Font.semiboldSystemFont(20);
+
+      listWidget.addSpacer(10);
+
+    });
+  } catch (error) {
+    console.log(`Could not render widget: ${error}`);
+
+    const errorWidgetText = listWidget.addText(`${error}`);
+    errorWidgetText.textColor = Color.red();
+    errorWidgetText.textOpacity = 30;
+    errorWidgetText.font = Font.regularSystemFont(10);
+  }
+
+  if (config.runsInApp) {
+    listWidget.presentSmall();
+  }
+
+  Script.setWidget(listWidget);
+  Script.complete();
+}
+
+await run();
